c7bbd2d8a745ab358b1f57b0b4df75e993b899f9
Álvaro Jesús Rodríguez Muñoz <alvarojesus1492@gmail.com>
2026-04-29 20:14:27 +0200

    Update variables.tf

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,11 +1,15 @@
 variable "project_id" {type = string}
+
 variable "region" {
   type    = string
   default = "europe-west3"
 }
+
 variable "apis" {
   type    = list(string)
   default = []
 }
+
 variable "bucket_name" {type = string}
+
 variable "repository_id" {type = string}
