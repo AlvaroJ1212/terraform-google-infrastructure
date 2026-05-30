7cba8d823d5835eb8a4b8b61aa603ac8515e43ce
Álvaro Jesús Rodríguez Muñoz <alvarojesus1492@gmail.com>
2026-05-30 21:34:44 +0200

    Update module sources to new GitHub repository

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,11 +1,11 @@
 module "apis" {
-  source     = "git::https://github.com/alvaro-gcp-platform/terraform-google-apis.git?ref=v1.0.0"
+  source     = "git::https://github.com/AlvaroJ1212/terraform-google-apis.git?ref=v1.0.0"
   project_id = var.project_id
   apis       = var.apis
 }
 
 module "network" {
-  source      = "git::https://github.com/alvaro-gcp-platform/terraform-google-network.git?ref=v1.0.0"
+  source      = "git::https://github.com/AlvaroJ1212/terraform-google-network.git?ref=v1.0.0"
   project_id  = var.project_id
   region      = var.region
   vpc_name    = "vpc-dev"
@@ -15,14 +15,14 @@ module "network" {
 }
 
 module "storage" {
-  source      = "git::https://github.com/alvaro-gcp-platform/terraform-google-storage.git?ref=v1.0.0"
+  source      = "git::https://github.com/AlvaroJ1212/terraform-google-storage.git?ref=v1.0.0"
   project_id  = var.project_id
   region      = var.region
   bucket_name = var.bucket_name
 }
 
 module "pubsub" {
-  source            = "git::https://github.com/alvaro-gcp-platform/terraform-google-pubsub.git?ref=v1.0.0"
+  source            = "git::https://github.com/AlvaroJ1212/terraform-google-pubsub.git?ref=v1.0.0"
   project_id        = var.project_id
   region            = var.region
   topic_name        = "my-topic-dev"
@@ -30,14 +30,14 @@ module "pubsub" {
 }
 
 module "repository" {
-  source        = "git::https://github.com/alvaro-gcp-platform/terraform-google-repository.git?ref=v1.0.0"
+  source        = "git::https://github.com/AlvaroJ1212/terraform-google-repository.git?ref=v1.0.0"
   project_id    = var.project_id
   repository_id = var.repository_id
   region        = var.region
 }
 
 module "monitoring" {
-  source                      = "git::https://github.com/alvaro-gcp-platform/terraform-google-monitoring.git?ref=v1.0.0"
+  source                      = "git::https://github.com/AlvaroJ1212/terraform-google-monitoring.git?ref=v1.0.0"
   project_id                  = var.project_id
   bucket_name                 = module.storage.bucket_name
   topic_name                  = module.pubsub.topic_name
@@ -47,7 +47,7 @@ module "monitoring" {
 }
 
 module "iam" {
-  source      = "git::https://github.com/alvaro-gcp-platform/terraform-google-iam.git?ref=v1.0.0"
+  source      = "git::https://github.com/AlvaroJ1212/terraform-google-iam.git?ref=v1.0.0"
   project_id  = var.project_id
   sa_roles    = ["roles/storage.admin", "roles/pubsub.admin"]
   bucket_name = module.storage.bucket_name
